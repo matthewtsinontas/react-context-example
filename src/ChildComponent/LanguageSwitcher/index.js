5e7d420f8f4cb2d5c3ffce42b02c28b2b76625da
Matthew Tsinontas <matthewtsinontas@gmail.com>
2021-01-07 17:14:37 +0000

    Tiny tidy ups

--- src/ChildComponent/LanguageSwitcher/index.js
+++ src/ChildComponent/LanguageSwitcher/index.js
@@ -4,19 +4,18 @@ import Wrapper from '../../Wrapper';
 
 // Because we wrap the default export for this component in our WithLang HOC, the props from that state
 // will be passed into here, essentially subscribing THIS component to state
-const LanguageSwitcher = ({ lang, setLang }) => {
-    return (
-        <Wrapper level={3}>
-            <p>I am a language switcher nested multiple levels in the component tree. I can set state using context!</p>
-            <select 
-                onChange={e => setLang(e.target.value)}
-                value={lang}
-                >
-                <option value="en">English</option>
-                <option value="ch">官话</option>
-            </select>
-        </Wrapper>
-    );
-}
+const LanguageSwitcher = ({ lang, setLang }) => (
+    <Wrapper level={3}>
+        <p>I am a language switcher nested multiple levels in the component tree. I can set state using context!</p>
+        <p>The current language is: {lang}</p>
+        <select 
+            onChange={e => setLang(e.target.value)}
+            value={lang}
+            >
+            <option value="en">English</option>
+            <option value="ch">官话</option>
+        </select>
+    </Wrapper>
+);
 
 export default WithLang(LanguageSwitcher);
